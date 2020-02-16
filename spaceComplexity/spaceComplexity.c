531e51d0b5bf13be2538ce72866ec4e378a98534
akash <theakashsarkar@gmail.com>
2020-02-16 19:24:28 +0600

    Space Complexity With cprogramming

---
+++ spaceComplexity/spaceComplexity.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+ int main()
+ {
+   int i,n,even[101];
+
+   for(i = 0; i < 101; i++)
+   {
+     even[i] = 0;
+   }
+   for(i = 0; i < 101; i+=2)
+   {
+     even[i] = 1;
+
+   }
+   scanf("%d",&n);
+   if(even[n])
+   {
+    printf("%d is even number",n);
+   }
+   else
+   {
+     printf("%d is odd number");
+   }
+   return 0;
+
+ }
